0b4562d222773f865cfcba64863dc70eae1c6ed5
mohammedfathaah <mohammedfathaah@gmail.com>
2025-08-07 21:56:04 +0530

    created function to add a new todo item

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ todoInput.addEventListener(`keypress`,function (e){
         console.log("cleared");
     }
 })
+
+//Function to add a new todo item
+function addTodo(todoText){
+    const todoItem=document.createElement(`li`);
+    todoItem.classList.add(`todo-item`,`flex`,`items-center`,`space-x-2`,`p-3`,`mb-2`,`bg-gray-50`,`rounded-md`,`border`,`border-gray-300`);
+  console.log("item1");
+    todoItem.innerhtml=`<input type="checkbox" class="todo-checkbox w-5 h-5">
+    <span class="todo-text flex-grow">${todoText}</span>
+    <button class="delete-btn bg-red-500 text-white py-1 px-3 rounded-md hover:bg-red-600">X</button>`;
+    //append the new items to the all section and active section
+    allTasks.appendChild(todoItem);
+    activeTasks.appendChild(todoItem);
+    
+}
